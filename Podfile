e195840fbe0de2cdf44229c333408208a598a621
ducaiyan <silinnayisha@163.com>
2016-12-28 10:31:41 +0800

    尝试命令行进行操作

---
+++ Podfile
@@ -0,0 +1,4 @@
+platform :ios, '7.0' 
+target '数据库和缓存'do
+pod 'FMDB'
+end
---
+++ 数据库和缓存/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  数据库和缓存
+//
+//  Created by ducaiyan on 16/12/27.
+//  Copyright © 2016年 ducaiyan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
